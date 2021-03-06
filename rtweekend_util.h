a0c64a99ba0c3ea05433e02f059d57eab750e818
EarthenSky <24978329+EarthenSky@users.noreply.github.com>
2021-03-06 11:59:35 -0800

    completed pt 7

--- rtweekend_util.h
+++ rtweekend_util.h
@@ -5,6 +5,7 @@
 #include <limits>
 #include <memory>
 
+#include <random>
 
 // Usings
 
@@ -22,6 +23,26 @@ const double pi = 3.1415926535897932385;
 // This is a good example of inline!
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+// TODO: replace this in C++17 w/ std
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
+
+// RNG Functions
+
+inline double random_double() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max - min) * random_double();
 }
 
 // Common Headers
